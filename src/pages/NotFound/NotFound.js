4fe50ae8a10a8668fbeb9174b2c0e9b5a8909ddd
anoninkriz <15214494+antoninkriz@users.noreply.github.com>
2020-08-14 04:17:31 +0200

    Improved copypasta

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -15,7 +15,7 @@ class NotFound extends React.Component {
         </Helmet>
         <h1 className="notfound__title">404</h1>
         <span className="notfound__oopsie">OOPSIE WOOPSIE!!</span>
-        <p className="notfound__fucky">Uwu we made a fucky wucky! A little fucko boingo! The code monekeys at our headquarters are working vewy hawd to fix this!</p>
+        <p className="notfound__fucky">UwU We made a fucky wucky!! A little fucko boingo! The code monekeys at our headquarters are working VEWY HAWD to fix this!</p>
         <a className="notfound__link" href="https://knowyourmeme.com/memes/oopsie-woopsie" target="_blank" rel="noopener noreferrer">what the fuck?</a>
       </section>
     );
